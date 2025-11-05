1916211547d37cb7d11291a8dbc4809214434faa
mdessler-tamu <mdessler@tamu.edu>
2025-11-04 19:19:11 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
-let http = require('http');
+require("./dist/server.js");
+
+/*let http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World!');
-}).listen(8080);
+
+}).listen(8080);*/
